283730489489f4e799779994a87521ddfe4cd6d4
Carey99 <edwinakidah1@gmail.com>
2024-01-22 08:25:48 -0800

    Hot fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,43 +8,44 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted, *current, *newNode, *temp;
+	listint_t *current, *newNode, *insertion_point;
 
-	sorted = NULL;
 	current = *list;
 
-	if (*list == NULL || (*list)->next == NULL)
-	{
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
-	}
+	current = (*list)->next;
 	while (current != NULL)
 	{
+		insertion_point = current->prev;
 		newNode = current->next;
-		if (sorted == NULL || sorted->n >= current->n)
+
+		while (insertion_point != NULL && insertion_point->n > current->n)
 		{
-			current->next = sorted;
+			insertion_point = insertion_point->prev;
+		}
+		if (insertion_point == NULL)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = *list;
 			current->prev = NULL;
-			if (sorted != NULL)
-			{
-				sorted->prev = current;
-			}
-			sorted = current;
+			(*list)->prev = current;
+			*list = current;
 		}
 		else
 		{
-			temp = sorted;
-			while (temp->next != NULL && temp->next->n < current->n)
-			{
-				temp = temp->next;
-			}
-			current->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = current;
-			temp->next = current;
-			current->prev = temp;
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = insertion_point->next;
+			current->prev = insertion_point;
+			if (insertion_point->next != NULL)
+				insertion_point->next->prev = current;
+			insertion_point->next = current;
 		}
 		print_list(*list);
 		current = newNode;
 	}
-	*list = sorted;
 }
