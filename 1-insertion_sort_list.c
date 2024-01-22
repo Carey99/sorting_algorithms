aa356baf55be2bd217b557115f451fcfa3e3c87a
Carey99 <edwinakidah1@gmail.com>
2024-01-22 09:47:36 -0800

    Selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,34 @@
 #include "sort.h"
 
 /**
- *  insertion_sort_list - Sortd a doubly linked list in ascending order
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * @list: Double pointer to the head of the linked list
  *
- *  @list: list containing data to bes sorted
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *newNode, *insertion_point;
+	listint_t *swapNode, *nextSwap;
 
-	current = *list;
-
-	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-	current = (*list)->next;
-	while (current != NULL)
+	swapNode = (*list)->next;
+	while (swapNode != NULL)
 	{
-		insertion_point = current->prev;
-		newNode = current->next;
-
-		while (insertion_point != NULL && insertion_point->n > current->n)
+		nextSwap = swapNode->next;
+		while (swapNode->prev != NULL && swapNode->prev->n > swapNode->n)
 		{
-			insertion_point = insertion_point->prev;
+			swapNode->prev->next = swapNode->next;
+			if (swapNode->next != NULL)
+				swapNode->next->prev = swapNode->prev;
+			swapNode->next = swapNode->prev;
+			swapNode->prev = swapNode->next->prev;
+			swapNode->next->prev = swapNode;
+			if (swapNode->prev == NULL)
+				*list = swapNode;
+			else
+				swapNode->prev->next = swapNode;
+			print_list(*list);
 		}
-		if (insertion_point == NULL)
-		{
-			current->prev->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->next = *list;
-			current->prev = NULL;
-			(*list)->prev = current;
-			*list = current;
-		}
-		else
-		{
-			current->prev->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->next = insertion_point->next;
-			current->prev = insertion_point;
-			if (insertion_point->next != NULL)
-				insertion_point->next->prev = current;
-			insertion_point->next = current;
-		}
-		print_list(*list);
-		current = newNode;
+		swapNode = nextSwap;
 	}
 }
